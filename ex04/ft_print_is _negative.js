abf6172bd81301aa29445813737086d196429217
emmamelanight <94955729+emmamelanight@users.noreply.github.com>
2023-01-25 14:47:04 +0100

    Add files via upload

---
+++ ex04/ft_print_is _negative.js
@@ -0,0 +1,9 @@
+function ft_print_is_negative(number) {
+        if (number === 0 || number > 0) {
+          return 'P';
+        }
+        else {
+          return 'N';
+        }
+      }
+      console.log('ft_print_is_negative', ft_print_is_negative(1), ft_print_is_negative(-1), ft_print_is_negative(0));
