1cc961be15fc9a6f3a77fd77fb537d6e5bc8a7ac
emmamelanight <94955729+emmamelanight@users.noreply.github.com>
2023-01-25 13:37:03 +0100

    exo1

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -1,4 +1,4 @@
 function ft_print_alphabet() {
-  return 'abcdefghijklmnopqrstuvwxyz';
+  console.log('abcdefghijklmnopqrstuvwxyz');
 }
-console.log('ft_print_alphabet', ft_print_alphabet());
+ft_print_alphabet();
